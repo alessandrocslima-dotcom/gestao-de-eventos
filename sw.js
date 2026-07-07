eec0820f910081c765f30fd87148ac8e90c6ba59
alessandrocslima-dotcom <alessandro.cslima@gmail.com>
2026-07-06 23:20:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker — GEP Gestão de Eventos Pro */
-const CACHE_VERSION = 'gep-v15-123';
+const CACHE_VERSION = 'gep-v15-124';
 const CACHE_NAME = 'gestao-eventos-' + CACHE_VERSION;
 
 const PRECACHE = [
